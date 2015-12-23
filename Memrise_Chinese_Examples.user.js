b8edce132d21bef435f6e8f6e9f4a09583da0456
Aaron Miles <cool_jingle@hotmail.co.uk>
2015-12-23 23:24:44 +0000

    added setCurrentWord to CopyTypingBox

--- Memrise_Chinese_Examples.user.js
+++ Memrise_Chinese_Examples.user.js
@@ -4,7 +4,7 @@
 // @description    Example sentences for learning Chinese on Memrise
 // @match          http://www.memrise.com/course/*/garden/*
 // @match          http://www.memrise.com/garden/review/*
-// @version        1.1.0
+// @version        1.1.1
 // @updateURL      https://github.com/cooljingle/memrise-chinese-examples/raw/master/Memrise_Chinese_Examples.user.js
 // @downloadURL    https://github.com/cooljingle/memrise-chinese-examples/raw/master/Memrise_Chinese_Examples.user.js
 // @grant          none
@@ -400,7 +400,8 @@
             showExample(true);
         });
 
-        addToBox("CopyTypingBox", function() {
+        addToBox("CopyTypingBox", function(context) {
+            setCurrentWord(context);
             showColouredWord();
             showExample(true);
         });
